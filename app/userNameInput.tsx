b059a37e67c0fceeb6561d8351c0ed48e7a27136
Nihar Hegde <niharhegde163@gmail.com>
2025-01-17 22:50:51 +0530

    add a simple submit button

--- app/userNameInput.tsx
+++ app/userNameInput.tsx
@@ -1,8 +1,17 @@
-import { View, Text, SafeAreaView, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 
 const UserNameInput = () => {
   const [userName, setUserName] = useState("");
+  const handlePress = () => {
+    console.log(userName);
+  };
   return (
     <SafeAreaView className="flex-1 bg-black">
       <View className="flex-1 flex flex-col gap-4 items-center justify-center p-4">
@@ -16,6 +25,12 @@ const UserNameInput = () => {
           value={userName}
           onChangeText={(text) => setUserName(text)}
         />
+        <TouchableOpacity
+          onPress={handlePress}
+          className="bg-gray-600 p-4 w-80 flex items-center justify-center rounded-full  "
+        >
+          <Text className="text-white text-xl font-bold">Submit</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
