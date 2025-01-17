2fdea05befe2426723a3f1647f8c2097464d9b07
Nihar Hegde <niharhegde163@gmail.com>
2025-01-17 23:13:09 +0530

    update design

--- app/userNameInput.tsx
+++ app/userNameInput.tsx
@@ -4,6 +4,8 @@ import {
   SafeAreaView,
   TextInput,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import React, { useState } from "react";
 
@@ -13,25 +15,39 @@ const UserNameInput = () => {
     console.log(userName);
   };
   return (
-    <SafeAreaView className="flex-1 bg-black">
-      <View className="flex-1 flex flex-col gap-4 items-center justify-center p-4">
-        <Text className="text-2xl font-bold text-white">
-          Enter Your Github UserName
-        </Text>
-        <TextInput
-          className="w-80 bg-white rounded-2xl"
-          placeholder="Enter Username..."
-          placeholderTextColor={"#000000"}
-          value={userName}
-          onChangeText={(text) => setUserName(text)}
-        />
-        <TouchableOpacity
-          onPress={handlePress}
-          className="bg-gray-600 p-4 w-80 flex items-center justify-center rounded-full  "
-        >
-          <Text className="text-white text-xl font-bold">Submit</Text>
-        </TouchableOpacity>
-      </View>
+    <SafeAreaView className="flex-1 bg-[#121212]">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        className="flex-1"
+      >
+        <View className="flex-1 items-center justify-center px-6 w-full">
+          {/* Titele Section */}
+          <Text className="text-3xl font-bold text-gray-100 mb-6">
+            Welcome To GitHub Stats!
+          </Text>
+          <Text className="text-lg text-gray-400 mb-8 text-center w-full">
+            Enter your GitHub UserName to explore your GitHub Stats!
+          </Text>
+
+          {/* Input Section */}
+          <TextInput
+            className="w-full bg-[#1E1E1E] text-white p-4 rounded-lg text-base"
+            placeholder="Enter your GitHub UserName..."
+            placeholderTextColor={"#666"}
+            value={userName}
+            onChangeText={(text) => setUserName(text)}
+            keyboardType="default"
+          />
+          {/* Button Section */}
+          <TouchableOpacity
+            onPress={handlePress}
+            disabled={!userName.trim()}
+            className={`${userName.trim() ? "bg-[#0066cc]" : "bg-[#333333]"} mt-6 w-full py-4 rounded-lg items-center`}
+          >
+            <Text className="text-white text-lg font-semibold">Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
